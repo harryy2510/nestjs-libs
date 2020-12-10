cceb5f90d6dd292f3059b4dd35d2a9d37e140525
Hariom Sharma <hariom@appointy.com>
2020-12-10 23:30:57 +0530

    feat: resolve nodes

--- libs/nestjs-relay/src/node/node-field.resolver.ts
+++ libs/nestjs-relay/src/node/node-field.resolver.ts
@@ -2,19 +2,31 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { NodeInterface } from './node.interface';
 import { GlobalId, GlobalIdType } from '../global-id';
 
+export type ResolvedNode = NodeInterface | undefined | null;
+
 export interface NodeResolver {
-  resolve(id: GlobalIdType): Promise<NodeInterface | undefined>;
+  resolveNode(id: GlobalIdType): Promise<ResolvedNode>;
 }
 
 @Resolver(NodeInterface)
 export class NodeFieldResolver implements NodeResolver {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  resolve(id: GlobalIdType): Promise<NodeInterface | undefined> {
+  resolveNode(id: GlobalIdType): Promise<ResolvedNode> {
+    throw new Error('Method not implemented.');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  resolveNodes(ids: GlobalIdType[]): Promise<ResolvedNode[]> {
     throw new Error('Method not implemented.');
   }
 
   @Query(() => NodeInterface, { name: 'node', nullable: true })
   node(@Args('id', { type: () => GlobalId }) id: GlobalIdType) {
-    return this.resolve(id);
+    return this.resolveNode(id);
+  }
+
+  @Query(() => [NodeInterface], { name: 'nodes', nullable: true })
+  nodes(@Args('ids', { type: () => [GlobalId] }) ids: GlobalIdType[]) {
+    return this.resolveNodes(ids);
   }
 }
