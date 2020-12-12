25762a6a99fe92f12114d73f8c4e166e40b90bba
Hariom Sharma <hariom@appointy.com>
2020-12-12 15:40:08 +0530

    chore: release process

--- scripts/clean-and-copy.js
+++ scripts/clean-and-copy.js
@@ -1,6 +1,14 @@
 const fs = require('fs-extra');
 const path = require('path');
-const rimraf = require('rimraf');
+
+const moveFilesAll = (srcDir, destDir) => {
+  fs.readdirSync(srcDir).forEach((file) => {
+    const destFile = path.join(destDir, file);
+    const srcFile = path.join(srcDir, file);
+    fs.renameSync(srcFile, destFile);
+  });
+  fs.removeSync(srcDir);
+};
 
 const packageJson = fs.readJsonSync(
   path.resolve(__dirname, '..', 'package.json'),
@@ -13,10 +21,9 @@ Object.keys(packageJson.dependencies).forEach((key) => {
   (key) => delete packageJson[key],
 );
 
-fs.readdirSync(path.resolve(__dirname, '..', 'dist')).forEach((name) => {
-  const targetPath = path.resolve(__dirname, '..', 'dist', name);
-  fs.move(path.resolve(targetPath, 'src'), targetPath, console.error);
-  rimraf(path.resolve(targetPath, 'src'));
+fs.readdirSync(path.resolve(__dirname, '..', 'build')).forEach((name) => {
+  const targetPath = path.resolve(__dirname, '..', 'build', name);
+  moveFilesAll(path.resolve(targetPath, 'src'), path.resolve(targetPath));
   const targetPackageJsonPath = path.resolve(targetPath, 'package.json');
   packageJson.name = `@harryy/${name}`;
   fs.outputJsonSync(targetPackageJsonPath, packageJson, { spaces: 2 });
