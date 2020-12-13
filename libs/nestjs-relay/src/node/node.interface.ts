808803ddfc8cdd82a6b94db1954089b6fc56bd22
Hariom Sharma <hariom@appointy.com>
2020-12-13 21:34:54 +0530

    fix: typings

--- libs/nestjs-relay/src/node/node.interface.ts
+++ libs/nestjs-relay/src/node/node.interface.ts
@@ -3,12 +3,15 @@ import { Field, ID, InterfaceType } from '@nestjs/graphql';
 import { NodeKey } from './node.key';
 
 @InterfaceType('Node')
-export class Node<T extends { id: string } = any> extends BaseEntity<T, 'id'> {
+export class Node<T extends { id: string } = any, U = any> extends BaseEntity<
+  T,
+  'id'
+> {
   @Field(() => ID)
   @NodeKey()
   id!: string;
 
-  constructor(input: T) {
+  constructor(input: U) {
     super();
     this.assign(input);
   }
