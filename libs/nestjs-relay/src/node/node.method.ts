3e0e3ce6044494922749568fa44335399683e584
Hariom Sharma <hariom@appointy.com>
2020-12-14 14:46:55 +0530

    fix: added id parsing function

--- libs/nestjs-relay/src/node/node.method.ts
+++ libs/nestjs-relay/src/node/node.method.ts
@@ -9,7 +9,27 @@ export function createNodeId(type: string) {
   return `${prefix}_${nanoid()}`;
 }
 
+export function parseId(id: string) {
+  const prefix = id.slice(0, 3);
+  const entityId = id.slice(4);
+  let type = '';
+  nodeStore.forEach((value, key) => {
+    if (value === prefix) {
+      type = key;
+    }
+  });
+  return {
+    id,
+    entityId,
+    type,
+    prefix,
+  };
+}
+
 export function registerNodePrefix(type: string, prefix: string) {
+  if (prefix.length !== 3) {
+    throw new Error(`Prefix - ${prefix} is not of length 3`);
+  }
   if (nodeStore.get(type)) {
     throw new Error(`Prefix for type - ${type} already exists`);
   }
--- libs/nestjs-relay/src/node/node.store.ts
+++ libs/nestjs-relay/src/node/node.store.ts
@@ -11,27 +11,6 @@ if (!global.store) {
   global.store = new Map<string, string>();
 }
 
-function clear() {
-  return global.store.clear();
-}
-
-function remove(key: string) {
-  return global.store.delete(key);
-}
-
-function get(key: string) {
-  return global.store.get(key);
-}
-
-function set(key: string, value: string) {
-  return global.store.set(key, value);
-}
-
-const nodeStore = {
-  get,
-  set,
-  remove,
-  clear,
-};
+const nodeStore = global.store;
 
 export default nodeStore;
